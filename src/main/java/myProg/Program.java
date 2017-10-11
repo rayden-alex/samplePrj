ace517b0232985dc3ae82befc0133a33417a7471
rayden <rayden@tut.by>
2017-10-11 23:33:15 +0300

    wewewew

--- src/main/java/myProg/Program.java
+++ src/main/java/myProg/Program.java
@@ -1,8 +1,30 @@
-package  myProg;
+package myProg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.logging.Logger;
 
 public class Program {
+    private static final Logger log = Logger.getLogger(Program.class.getName());
+
     public static void main(String[] args) {
         Calc calc = new Calc();
         System.out.println(calc.add(23, 34));
+
+        Person personAlice = new Person("Alice", 18);
+        Person personBob = new Person("Bob", 28);
+
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(personAlice);
+        list.add(personBob);
+
+        Person[] sortedArray = list.toArray(new Person[list.size()]);
+        Arrays.sort(sortedArray, Comparator.comparing(Person::getName));
+
+        System.out.println("logggggggg");
+        log.severe("Test logger message");
+
+
     }
 }
