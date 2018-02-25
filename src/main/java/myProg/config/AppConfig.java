e58067ff63565f1844d52dbe9fa4eb828010c320
rayden <rayden@tut.by>
2018-02-26 00:42:29 +0300

    Added CsvMapper bean (with LocalDateTime processing)

--- src/main/java/myProg/config/AppConfig.java
+++ src/main/java/myProg/config/AppConfig.java
@@ -1,8 +1,12 @@
 package myProg.config;
 
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
@@ -12,4 +16,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Import(DataBaseConfig.class)
 public class AppConfig {
 
+    @NonNull
+    @Bean
+    public CsvMapper csvFileMapper() {
+        return (CsvMapper) new CsvMapper().registerModule(new JavaTimeModule()); // new module, NOT JSR310Module
+    }
 }
